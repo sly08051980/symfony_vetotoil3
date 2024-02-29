bd3e1d1ee6a99ccd4751787a5176ff4a13be04e8
Sylvain Regnier <regnier.sylvain@yahoo.fr>
2024-02-29 13:28:08 +0100

    avantmail

--- src/Controller/LogginPatientController.php
+++ src/Controller/LogginPatientController.php
@@ -1,40 +1,45 @@
 <?php
 
 namespace App\Controller;
-use App\Form\PatientType;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LogginPatientController extends AbstractController
 {
     #[Route('/patient/loggin', name: 'app_loggin')]
-    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // Récupérer les erreurs d'authentification, s'il y en a
         $error = $authenticationUtils->getLastAuthenticationError();
-
-        // Récupérer le dernier nom d'utilisateur (email) saisi par l'utilisateur
+       
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        // Créer le formulaire de connexion
-        $form = $this->createForm(PatientType::class);
-
-        // Gérer la soumission du formulaire de connexion
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            // Si le formulaire est soumis et valide, vous pouvez traiter l'authentification ici
-            // Par exemple, vous pouvez utiliser le composant de sécurité de Symfony pour gérer l'authentification
-            // En fonction de votre configuration de sécurité, Symfony gérera automatiquement l'authentification et redirigera l'utilisateur vers la page appropriée
-        }
-
-        // Rendre le template avec le formulaire de connexion
         return $this->render('loggin_patient/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
-            'form' => $form->createView(),
+            
         ]);
     }
+
+    public function secutitePatient(Security $security)
+    {
+        $user = $security->getUser();
+
+      
+        if ($user instanceof \App\Entity\Patient) {
+         
+            $nom = $user->getNomPatient();
+            $prenom = $user->getPrenomPatient();
+            $email = $user->getEmail();
+            
+        }
+        
+
+
+    }
 }
+
+
